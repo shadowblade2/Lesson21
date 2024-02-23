9c660c34e37ef67428952be960ebc15383493bf7
shadowblade <155826618+shadowblade2@users.noreply.github.com>
2024-02-23 16:33:17 +0000

    Додали малювання кола по клікові мишки

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function draw(event) {
     let canvas = document.getElementById("imgCanvas");
+    let ctx = canvas.getContext("2d");
+    let rect = canvas.getBoundingClientRect();
+
+    let posX = event.clientX;
+    let posY = event.clientY;
+
+    ctx.fillStyle = "#00ff00"
+
+    ctx.arc(posX, posY, 50, 0, 2 * Math.PI);
+
+    
 }
