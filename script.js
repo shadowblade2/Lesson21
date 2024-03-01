8840db3bd174d74a3878579a36024c8332c52bb0
Misha <sharunovm@gmail.com>
2024-03-01 18:11:10 +0200

    Реалізація малювання кола по кліку - перевірка

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function draw(event) {
     let canvas = document.getElementById("imgCanvas");
+    let ctx = canvas.getContext("2d");
+    let rect = canvas.getBoundingClientRect();
+
+    let posX = event.clientX - rect.left;
+    let posY = event.clientY - rect.top;
+
+    ctx.fillStyle = "#00ff00";
+
+    ctx.beginPath();
+
+    ctx.arc(posX, posY, 50, 0, 2 * Math.PI);
+
+    ctx.fill();
 }
